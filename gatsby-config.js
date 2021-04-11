c938c0fe1a48735f159f20f50cbea264cc67c96d
Rocky <rockynhatnguyen@gmail.com>
2021-04-11 11:57:13 -0700

    update: added Google Fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,15 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    }
   ],
 }
